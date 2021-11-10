d5c2ccc752c3ab5aa7f6d3e17d6ab93b17acbe09
BK Uddin <1bkhossain@gmail.com>
2021-11-11 00:54:28 +0600

    Updated Login Form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Alert, Button, Form, Spinner } from "react-bootstrap";
 import { NavLink, useLocation, useHistory } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
+import "./Login.css";
 
 const Login = () => {
   const { user, loginUser, isLoading, authError } = useAuth();
@@ -23,46 +24,52 @@ const Login = () => {
     setLoginData(newLoginData);
   };
   return (
-    <div>
-      <Form
-        onSubmit={handleLoginSubmit}
-        className="w-25"
-        style={{ margin: "0 auto" }}
-      >
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            name="email"
-            onChange={handleOnChange}
-            type="email"
-            placeholder="Enter email"
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <div className="login-bg">
+      <div className="login-form">
+        <Form
+          onSubmit={handleLoginSubmit}
+          className="w-50"
+          style={{ margin: "0 auto" }}
+        >
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label></Form.Label>
+            <Form.Control
+              name="email"
+              onChange={handleOnChange}
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-light">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label></Form.Label>
+            <Form.Control
+              name="password"
+              onChange={handleOnChange}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            name="password"
-            onChange={handleOnChange}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Form.Group>
-          <NavLink to="/register" style={{ textDecoration: "none" }}>
-            <button className="border-0 mb-2 bk-button">
+          <button className="bk-button w-100" type="submit">
+            Login
+          </button>
+
+          <Form.Group className="mt-2">
+            <NavLink
+              to="/register"
+              style={{
+                textDecoration: "none",
+                color: "white",
+              }}
+            >
               New User? Please Register
-            </button>
-          </NavLink>
-        </Form.Group>
-
-        <Button className="bk-button w-100" type="submit">
-          Login
-        </Button>
-      </Form>
+            </NavLink>
+          </Form.Group>
+        </Form>
+      </div>
 
       {isLoading && <Spinner animation="border" variant="success" />}
       {user?.email && (
