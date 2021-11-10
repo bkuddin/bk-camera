c13a9daac5edc5addcf95456f6d52cac55621c90
BK Uddin <1bkhossain@gmail.com>
2021-11-10 11:25:10 +0600

    Defined Header

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,32 +1,24 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import Cover1 from "../../../images/cover-1.jpg";
+import Cover2 from "../../../images/cover-2.jpg";
+import Cover3 from "../../../images/cover-3.jpg";
 
 const Banner = () => {
   return (
     <div>
       <Carousel variant="dark">
         <Carousel.Item>
-          <img className="d-block w-100" src="" alt="First slide" />
-          <Carousel.Caption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+          <img className="d-block w-100" src={Cover1} alt="First slide" />
+          <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src="" alt="Second slide" />
-          <Carousel.Caption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          <img className="d-block w-100" src={Cover2} alt="Second slide" />
+          <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src="" alt="Third slide" />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+          <img className="d-block w-100" src={Cover3} alt="Third slide" />
+          <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
