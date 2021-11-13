51bcb32ae325c4ac4418531fa01a4be7f9a2dc52
BK Uddin <1bkhossain@gmail.com>
2021-11-13 12:09:35 +0600

    Defined Home Page Responsive

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,66 +3,78 @@ import "./Footer.css";
 import { Col, Container, Row } from "react-bootstrap";
 
 // React Icons
-import { FaFacebookSquare, FaTwitterSquare } from "react-icons/fa";
+import { FaFacebookSquare, FaTwitterSquare, FaGithub } from "react-icons/fa";
+import logo from "../../../images/logo-white.png";
 
 const Footer = () => {
   return (
     <div>
       <Container className="footer" fluid>
-        <Row>
-          <Col>
-            <FaFacebookSquare />
-            <FaTwitterSquare />
-          </Col>
-        </Row>
-        <Row xs={1} sm={1} md={4}>
-          <Col>
-            <h3>BK CAMERA</h3>
-            <p>
-              BK Camera is a significant supplier of digital imaging solutions
-              for the consumer, business-to-business, and industrial markets.
-            </p>
-          </Col>
-          <Col>
-            <h5 className="title">Company</h5>
-            <ul>
-              <li className="list-unstyled">
-                <a href="#!">About</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">News</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Sustainability</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Site Map</a>
-              </li>
-            </ul>
-          </Col>
-          <Col>
-            <h5 className="title">Legal</h5>
-            <ul>
-              <li className="list-unstyled">
-                <a href="#!">Product Advisories</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Terms of Use</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Privacy Statement</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">To Our Customers</a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-        <Row className="copyright">
-          <Col>
-            &copy; {new Date().getFullYear()} Copyright:{" "}
-            <a href="https://www.bkcamera.com"> BKcamera.com </a>
-          </Col>
+        <Row className="footer-section">
+          <Row className="pt-5 social-icons">
+            <Col>
+              <a href="https://www.facebook.com/bakhtiar.uddins/">
+                <FaFacebookSquare />
+              </a>
+              <span className="mx-2">
+                <a href="https://www.facebook.com/bakhtiar.uddins/">
+                  <FaTwitterSquare />
+                </a>
+              </span>
+              <a href="https://github.com/bkuddin">
+                <FaGithub />
+              </a>
+            </Col>
+          </Row>
+          <Row className="footer-text">
+            <Col xs={12} sm={12} md={6}>
+              <img style={{ width: "50%" }} src={logo} alt="" />
+              <p>
+                BK Camera is a significant supplier of digital imaging solutions
+                for the consumer, business-to-business, and industrial markets.
+              </p>
+            </Col>
+            <Col xs={12} sm={12} md={3}>
+              <ul>
+                <h5 className="title">Company</h5>
+                <li className="list-unstyled">
+                  <a href="#">About</a>
+                </li>
+                <li className="list-unstyled">
+                  <a href="#">News</a>
+                </li>
+                <li className="list-unstyled">
+                  <a href="#">Sustainability</a>
+                </li>
+                <li className="list-unstyled">
+                  <a href="#">Site Map</a>
+                </li>
+              </ul>
+            </Col>
+            <Col xs={12} sm={12} md={3}>
+              <ul>
+                <h5 className="title">Legal</h5>
+                <li className="list-unstyled">
+                  <a href="#!">Product Advisories</a>
+                </li>
+                <li className="list-unstyled">
+                  <a href="#!">Terms of Use</a>
+                </li>
+                <li className="list-unstyled">
+                  <a href="#!">Privacy Statement</a>
+                </li>
+                <li className="list-unstyled">
+                  <a href="#!">To Our Customers</a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+          <Row className="copyright">
+            <Col>
+              &copy; {new Date().getFullYear()} Copyright:
+              <a href="https://www.bkcamera.com"> BKcamera.com </a>
+            </Col>
+          </Row>
         </Row>
       </Container>
     </div>
